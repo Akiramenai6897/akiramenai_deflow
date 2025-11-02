8d44bd38ab812dcf772a477f6ae27a6028df65a7
Haider <haider.sm6897@yahoo.com>
2025-11-03 03:31:52 +0500

    Tailwind CSS Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
   return (
     <div>
-      <h1 className="text-3xl">Welcome to the Ultimate Next.js course!</h1>
+      <h1 className="text-light-500 text-3xl">Welcome to the Ultimate Next.js course!</h1>
     </div>
   );
 };
