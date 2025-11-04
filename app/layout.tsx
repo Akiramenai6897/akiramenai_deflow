8a6e307b399adb41fbc7236012f9be552f2e71f8
Haider <haider.sm6897@yahoo.com>
2025-11-04 23:43:12 +0500

    Setup next-themes Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import AppThemeProvider from "../context/ThemeProvider";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -29,8 +30,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+        <AppThemeProvider attribute="class" defaultTheme="system" enableSystem={true} disableTransitionOnChange={true}>
+          {children}
+        </AppThemeProvider>
+      </body>
     </html>
   );
 }
